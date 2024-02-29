82ed07ddcf83998176208d656a6f28b69e8b2341
Yongmalyang <37902200+Yongmalyang@users.noreply.github.com>
2024-02-29 10:33:01 +0900

    Update 10757.cpp

--- 10757.cpp
+++ 10757.cpp
@@ -11,39 +11,55 @@ int main()
 
     string a, b;
     cin >> a >> b;
+    int lenA = a.length();
+    int lenB = b.length();
 
     stack<int> s_a;
     stack<int> s_b;
-
-    for(int i=0; i<a.length(); i++){ s_a.push(a[i]-'0'); }
-    for(int i=0; i<b.length(); i++){ s_b.push(b[i]-'0'); }
-
     stack<int> answer;
 
-    while(!s_a.empty() || !s_b.empty())
-    {
-        int sum = s_a.top() + s_b.top(); 
-        answer.push(sum % 10);
-        int carry = sum/10;
-        
-        s_a.pop();
-        s_b.pop();
-        
-        if(s_a.empty() && carry > 0)
-        {
-            answer.push(carry);
-        }
-        else
-        {
-            int temp = s_a.top();
-            s_a.pop();
-            s_a.push(temp+carry);
-        }
+    if (lenB > lenA) {
+        swap(a, b);
+        swap(lenA, lenB);
     }
 
-    for(int i=0; i<answer.size(); i++)
+    if (lenA != lenB){
+        for (int i=0; i < (lenA - lenB); i++)
+            s_b.push(0);
+    }
+
+    for(int i=0; i<a.length(); i++){ int tmp = a[i]-'0'; s_a.push(tmp); }
+    for(int i=0; i<b.length(); i++){ int tmp = b[i]-'0'; s_b.push(tmp); }
+
+    int carry = 0; 
+    int sum; 
+    
+    for(int i=0; i < lenA; i++)
+    {
+        sum = s_a.top() + s_b.top();
+        
+        if(carry==1)
+        {
+            sum++;
+            carry = 0;
+        }
+        if(sum > 9)
+        {
+            carry = 1;
+        }
+
+        answer.push(sum % 10);
+
+        s_a.pop();
+        s_b.pop();
+    }
+
+    if(carry==1) {answer.push(1);}
+    
+    while(!answer.empty())
     {
         cout << answer.top();
         answer.pop();
     }
+
 }
